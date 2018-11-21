49bdcff02679beef87c47915f7745eb4a7f76375
Maksym <maxwell88soltyk@gmail.com>
2018-11-22 01:12:53 +0200

    Forget to handle one case

--- src/ServiceLayer.js
+++ src/ServiceLayer.js
@@ -12,7 +12,7 @@ export default class ServiceLayer {
                 const validArgs = await this._executeRules(ServiceClass, ctx);
                 const service = new ServiceClass();
                 const data = await service.execute(validArgs);
-                
+
                 ctx.body = { status: 200, ...data };
             } catch (error) {
                 if (error instanceof Exception) {
@@ -32,11 +32,10 @@ export default class ServiceLayer {
         for (const rule of this.rules) {
             const ruleArgs = ServiceClass[rule.name];
             const { name, body, type } = rule;
-
-            if (!ServiceClass[name] && type === "required") {
-                throw new Exception({ code: "RULE_IS_REQUIRED", fields: { rule: name } });
-            } else {
+            if (type) {
                 changedCtx = await body(changedCtx, ruleArgs);
+            } else if (!ServiceClass[name] && type === "required") {
+                throw new Exception({ code: "RULE_IS_REQUIRED", fields: { rule: name } });
             }
         }
     }
