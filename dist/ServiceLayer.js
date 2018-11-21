1d8ca86e76d429ca43420b34f1b24b4ed1f2f9f4
Maksym <maxwell88soltyk@gmail.com>
2018-11-22 01:07:43 +0200

    Change architecture. Refuse logger, move to rules loger. Add type of rules that can be required, hidden or undefined

--- dist/ServiceLayer.js
+++ dist/ServiceLayer.js
@@ -11,66 +11,42 @@ var _Exception2 = _interopRequireDefault(_Exception);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 class ServiceLayer {
-    constructor({ logger, rules }) {
+    constructor({ rules }) {
         this.logger = logger;
         this.rules = rules;
     }
 
-    // logRequest(type, result, startTime) {
-    //     this.logger(type, {
-    //         service: serviceClass.name,
-    //         runtime: Date.now() - startTime,
-    //         params: inspect(params, { showHidden: false, depth: null }),
-    //         result
-    //     });
-    // }
-
     useService(ServiceClass) {
         return async ctx => {
-            const validArgs = await this._executeRules(ServiceClass, ctx);
-            const startTime = Date.now();
-            const service = new ServiceClass();
-            const data = await service.execute(validArgs);
+            try {
+                const validArgs = await this._executeRules(ServiceClass, ctx);
+                const service = new ServiceClass();
+                const data = await service.execute(validArgs);
 
-            return this._handleService({ ctx, data, meta: { startTime } });
+                ctx.body = _extends({ status: 200 }, data);
+            } catch (error) {
+                if (error instanceof _Exception2.default) {
+                    ctx.body = { status: 500, error: error.toHash() };
+                } else {
+                    ctx.body = { status: 500, error: { code: 'UNKNOWN_ERROR' } };
+                }
+            }
         };
     }
 
     async _executeRules(ServiceClass, ctx) {
         let changedCtx = ctx;
-
+        // rules type can be required, epty, hidden
         for (const rule of this.rules) {
             const ruleArgs = ServiceClass[rule.name];
-            if (ruleArgs) {
-                changedCtx = await rule.body(changedCtx, ruleArgs);
-            } else if (!ServiceClass[rule.name] && rule.required) {
-                throw new _Exception2.default({
-                    code: "RULE_IS_REQUIRED",
-                    fields: {
-                        rule: rule.name
-                    }
-                });
-            }
-        }
-    }
+            const { name, body, type } = rule;
 
-    _handleService({ ctx, data }) {
-        try {
-            ctx.body = _extends({ status: 200 }, data);
-        } catch (error) {
-            if (error instanceof _Exception2.default) {
-                res.send({
-                    status: 500,
-                    error: error.toHash()
-                });
+            if (!ServiceClass[name] && type === "required") {
+                throw new _Exception2.default({ code: "RULE_IS_REQUIRED", fields: { rule: name } });
             } else {
-                res.send({
-                    status: 500,
-                    error: { code: 'UNKNOWN_ERROR' }
-                });
+                changedCtx = await body(changedCtx, ruleArgs);
             }
         }
     }
-
 }
 exports.default = ServiceLayer;
--- src/ServiceLayer.js
+++ src/ServiceLayer.js
@@ -1,70 +1,44 @@
 import Exception from './Exception';
 
 export default class ServiceLayer {
-    constructor({ logger, rules }) {
+    constructor({ rules }) {
         this.logger = logger;
         this.rules = rules;
     }
 
-    // logRequest(type, result, startTime) {
-    //     this.logger(type, {
-    //         service: serviceClass.name,
-    //         runtime: Date.now() - startTime,
-    //         params: inspect(params, { showHidden: false, depth: null }),
-    //         result
-    //     });
-    // }
-
     useService(ServiceClass) {
         return async ctx => {
-            const validArgs = await this._executeRules(ServiceClass, ctx);
-            const startTime = Date.now();
-            const service = new ServiceClass();
-            const data = await service.execute(validArgs);
+            try {
+                const validArgs = await this._executeRules(ServiceClass, ctx);
+                const service = new ServiceClass();
+                const data = await service.execute(validArgs);
+                
+                ctx.body = { status: 200, ...data };
+            } catch (error) {
+                if (error instanceof Exception) {
+                    ctx.body = { status: 500, error: error.toHash() };
 
-            return this._handleService({ ctx, data, meta: { startTime } })
+                } else {
+                    ctx.body = { status: 500, error: { code: 'UNKNOWN_ERROR' } };
+                }
+
+            }
         }
     }
 
     async _executeRules(ServiceClass, ctx) {
         let changedCtx = ctx;
-
+        // rules type can be required, epty, hidden
         for (const rule of this.rules) {
             const ruleArgs = ServiceClass[rule.name];
-            if (ruleArgs) {
-                changedCtx = await rule.body(changedCtx, ruleArgs);
-            } else if (!ServiceClass[rule.name] && rule.required) {
-                throw new Exception({
-                    code: "RULE_IS_REQUIRED",
-                    fields: {
-                        rule: rule.name
-                    }
-                });
-            }
-        }
-    }
-    
-    _handleService({ ctx, data }) {
-        try {
-            ctx.body = { status: 200, ...data };
-        } catch (error) {
-            if (error instanceof Exception) {
-                res.send({
-                    status: 500,
-                    error: error.toHash()
-                });
+            const { name, body, type } = rule;
 
+            if (!ServiceClass[name] && type === "required") {
+                throw new Exception({ code: "RULE_IS_REQUIRED", fields: { rule: name } });
             } else {
-                res.send({
-                    status: 500,
-                    error: { code: 'UNKNOWN_ERROR' }
-                });
+                changedCtx = await body(changedCtx, ruleArgs);
             }
-
         }
     }
-
-   
-
 }
 
